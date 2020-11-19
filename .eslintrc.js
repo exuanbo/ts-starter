3d6572d36ea9cc312ec5ab1e050acead5acdfbb6
exuanbo <exuanbo@protonmail.com>
2020-11-19 12:32:32 +0000

    chore: rm unused comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,3 @@
 const { mergeObj } = require('standard-engine-ts')
 
-module.exports = mergeObj(require('ts-standardx/.eslintrc.js'), {
-  // settings: {
-  //   react: {
-  //     pragma: 'h',
-  //     version: 'detect'
-  //   }
-  // }
-})
+module.exports = mergeObj(require('ts-standardx/.eslintrc.js'), {})
